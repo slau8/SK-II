67385ff9c5b5b23b00e01a68c37ec042b9d96f6c
Khyber Sen <kkysen@gmail.com>
2017-09-15 13:19:21 -0400

    removed Total occupation at the end

--- occupations.py
+++ occupations.py
@@ -51,7 +51,7 @@ class Occupations(object):
     @staticmethod
     def parse_line(line):
         """
-        Parses a line from the occupations.csv file,
+        Parse a line from the occupations.csv file,
         where the first column is the occupation name
         (optionally quoted) and the second is the
         out of 100 percent for that occupation.
@@ -64,19 +64,19 @@ class Occupations(object):
 
     def __init__(self, file_name):
         """
-        Creates an Occupations object containing a list
+        Create an Occupations object containing a list
         of the occupation, percent pairs, as well as
         the total percent, used to calculate weighted randomness.
         :param file_name: str
         """
         # type: str -> Occupations
         self.occupations = [Occupations.parse_line(line) for line in
-                            open(file_name).read().splitlines()[1:]]
+                            open(file_name).read().splitlines()[1:-1]]
         self.total_percent = sum(occupation[1] for occupation in self.occupations)
 
     def random_occupation(self):
         """
-        Returns a random occupation weighted by the percents of each.
+        Return a random occupation weighted by the percents of each.
         Occupations with higher percents are more likely to be returned.
         :return: a weighted random occupation
         """
@@ -84,12 +84,13 @@ class Occupations(object):
         random_percent = random.uniform(0, self.total_percent)  # type: float
         for occupation, percent in self.occupations:
             if random_percent < percent:
+                print(occupation)
                 return occupation
             random_percent -= percent
 
     def random_occupations(self, num_occupations):
         """
-        Returns a Counter of num_occupations chosen weighted randomly
+        Return a Counter of num_occupations chosen weighted randomly
         as by random_occupation.
         :param num_occupations: the number of occupations to return
         :return: a Counter of the random occupations
@@ -99,7 +100,7 @@ class Occupations(object):
 
     def is_randomly_weighted(self, sample_size=DEFAULT_SAMPLE_SIZE, debug=False):
         """
-        Determines if the random_occupation method is working correctly
+        Determine if the random_occupation method is working correctly
         by sampling randomly weighted occupations and then comparing
         their statistical distribution with the percents by which
         the occupations are supposed to be weighted.
@@ -107,7 +108,7 @@ class Occupations(object):
         :param debug: if the resulting chosen occupations
          and their statistical percents should be printed
         :return: true if the random_occupation method
-        correctly returns random occupations weighted by their percents
+        correctly Return random occupations weighted by their percents
         """
         # type: (int, bool) -> bool
         count = self.random_occupations(sample_size)
@@ -127,7 +128,7 @@ class Occupations(object):
     @staticmethod
     def in_united_states():
         """
-        Gets an Occupations singleton from 'occupations.csv',
+        Get an Occupations singleton from 'occupations.csv',
         which contains occupation data from the United States.
         :return: an Occupations singleton for the United States
         """
@@ -139,7 +140,7 @@ class Occupations(object):
     @staticmethod
     def test(sample_size=DEFAULT_SAMPLE_SIZE, debug=True):
         """
-        Tests if the United States Occupations is randomly_weighted.
+        Test if the United States Occupations is randomly_weighted.
         If not, it throws an AssertionError
         :param sample_size: the number of occupations to sample
         :param debug: if debug info should be printed
@@ -153,7 +154,7 @@ class Occupations(object):
 
 def test(sample_size=Occupations.DEFAULT_SAMPLE_SIZE, debug=True):
     """
-    Calls Occupations.test
+    Call Occupations.test
     :param sample_size: the number of occupations to sample
     :param debug: if debug info should be printed
     """
@@ -163,7 +164,7 @@ def test(sample_size=Occupations.DEFAULT_SAMPLE_SIZE, debug=True):
 
 def main():
     """
-    Runs test for sample sizes from 1e2 to 1e6 in powers of 10
+    Run test for sample sizes from 1e2 to 1e6 in powers of 10
     """
     # type: () ->
     million = True
